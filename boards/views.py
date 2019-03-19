9d3d3c6569dd4b6364b5e9025a1a93e97869dd5b
jeongSangyoung <mycampground@daum.net>
2019-03-19 03:17:21 +0000

    09|CRUD-U|

--- boards/views.py
+++ boards/views.py
@@ -26,10 +26,11 @@ def create(request):
             return redirect(board)
     else:
         board_form = BoardForm()
+        
     context = {
         'board_form':board_form
         }
-    return render(request,'boards/create.html',context)
+    return render(request,'boards/form.html',context)
     
 def detail(request,board_pk):
     # board = Board.objects.get(pk=board_pk) #사용자가 없는 값을 입력하면 에러가 떠, 그래서 get_object_or_404를 import 해줘
@@ -48,3 +49,29 @@ def delete(request,board_pk):
         return redirect('boards:index')
     else:
         return redirect(board)
+        
+def update(request, board_pk):
+    # 1. board_pk에 해당하는 오브젝트를 가져온다.
+    #    - 없으면, 404
+    #    - 있으면, board = Board.objects.get(pk=board_pk) 와 동일
+    board = get_object_or_404(Board, pk=board_pk)
+    
+    # 2-1. POST 요청이면 (사용자가 form을 통해 데이터를 보내 준 것.)
+    if request.method == "POST":
+        # 사용자 입력값(request.POST)을 BoardForm에 전달해주고, 
+        board_form = BoardForm(request.POST)
+        # 검증 (유효성 체크)
+        if board_form.is_valid():
+            board.title = board_form.cleaned_data.get('title')
+            board.content = board_form.cleaned_data.get('content')
+            board.save()
+            return redirect(board)
+    # 2-2. GET 요청이면 (수정하기 버튼을 눌렀을 때)
+    else:
+        # BoardForm을 초기화 (사용자 입력값을 넣어준 상태)
+        board_form = BoardForm(initial=board.__dict__)
+    
+    context = {
+        'board_form': board_form
+    }    
+    return render(request,'boards/form.html',context)
