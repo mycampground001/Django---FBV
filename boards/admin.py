16365fc94ddc80972f1216e5fd1d1ac6409c3a72
jeongSangyoung <mycampground@daum.net>
2019-03-19 00:25:51 +0000

    03|Board model 생성|

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Board
 # Register your models here.
+
+class BoardAdmin(admin.ModelAdmin):
+    # 표 형태로 출력하기 위해 클래스 작성
+    list_display = ['id','title','content','hit','created_at','updated_at']
+
+
+admin.site.register(Board, BoardAdmin)
