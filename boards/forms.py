4a7bae2d36d4fb78497a36ec0a7844244c8a350e
jeongSangyoung <mycampground@daum.net>
2019-03-19 04:52:50 +0000

    10|ModelForm 적용|

--- boards/forms.py
+++ boards/forms.py
@@ -1,16 +1,41 @@
 from django import forms
 from .models import Board
 
-class BoardForm(forms.Form):
-    title = forms.CharField(label="제목", max_length=10,
-                            error_messages={"required":"제목써라"}
-                            )
+# class BoardForm(forms.Form):
+#     title = forms.CharField(label="제목", max_length=10,
+#                             error_messages={"required":"제목써라"}
+#                             )
                             
                             
-    content = forms.CharField(label="내용", 
-                            error_messages={"required":"내용써라"},
-                            widget=forms.Textarea(attrs={
-                                'placeholder':"내용쓰자",
-                                'class':'input-box'
-                            })
-                            )
+#     content = forms.CharField(label="내용", 
+#                             error_messages={"required":"내용써라"},
+#                             widget=forms.Textarea(attrs={
+#                                 'placeholder':"내용쓰자",
+#                                 'class':'input-box'
+#                             })
+#                             )
+                            
+# modelform
+# 중복제거를 위한. 모델이 있는경우 이런경우는 편하게...
+class BoardForm(forms.ModelForm):
+    class Meta:
+        model = Board
+        # fields = '__all__'
+        fields = ['title','content']
+        widgets = {'title': forms.TextInput(attrs={
+                                            'placeholder':'제목써라',
+                                            'class':'title'
+                                            }),
+                    'content':forms.Textarea(attrs={
+                                            'placeholder':'내용써라',
+                                            'class':'content'
+                                            })
+                    }
+    
+        error_messages = {'title':{
+                                'required':'제목은 반드시 써라'
+                                },
+                            'content': {
+                                    'required':'내용은 반드시 써라'
+                            }
+                        }
--- boards/views.py
+++ boards/views.py
@@ -19,10 +19,11 @@ def create(request):
         # title = request.POST.get('title')
         # content = request.POST.get('content')
         if board_form.is_valid(): #유효하면
-            title = board_form.cleaned_data.get('title') #정제된 데이터를 가져온다.
-            content = board_form.cleaned_data.get('content')
-            board = Board(title=title,content=content)
-            board.save()
+            # title = board_form.cleaned_data.get('title') #정제된 데이터를 가져온다.
+            # content = board_form.cleaned_data.get('content')
+            # board = Board(title=title,content=content)
+            # board.save()
+            board = board_form.save() #위의 주석4줄과 동일함.
             return redirect(board)
     else:
         board_form = BoardForm()
@@ -59,18 +60,22 @@ def update(request, board_pk):
     # 2-1. POST 요청이면 (사용자가 form을 통해 데이터를 보내 준 것.)
     if request.method == "POST":
         # 사용자 입력값(request.POST)을 BoardForm에 전달해주고, 
-        board_form = BoardForm(request.POST)
+        board_form = BoardForm(request.POST, instance=board)
         # 검증 (유효성 체크)
         if board_form.is_valid():
-            board.title = board_form.cleaned_data.get('title')
-            board.content = board_form.cleaned_data.get('content')
-            board.save()
+            # board.title = board_form.cleaned_data.get('title')
+            # board.content = board_form.cleaned_data.get('content')
+            # board.save()
+            board = board_form.save() # 위 주석 3줄과 동일
             return redirect(board)
     # 2-2. GET 요청이면 (수정하기 버튼을 눌렀을 때)
     else:
         # BoardForm을 초기화 (사용자 입력값을 넣어준 상태)
-        board_form = BoardForm(initial=board.__dict__)
-    
+        # board_form = BoardForm(initial=board.__dict__)
+        board_form = BoardForm(instance=board)
+    # context에 담겨있는 board_form은 두 가지 상황이 있다.
+    # 1 - POST 요청에서 검증에 실패하였을 때, 오류 메세지가 포함된 상태
+    # 2 - GET 요청에서 초기화된 상태
     context = {
         'board_form': board_form
     }    
